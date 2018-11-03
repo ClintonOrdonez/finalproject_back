f58523eea8e97f7d6be4c0a1ce335c90f8b3a3a8
Clinton Dean Ordoñez <dasmexikaner@Clintons-iMac.local>
2018-11-02 17:00:54 -0700

    All instances of "users" now read "user"

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 let router = require("express").Router();
 
 // router.use("/secrets", require("./secretRoute"));
-router.use("/users", require("./userRoute"));
+router.use("/user", require("./userRoute"));
 
 module.exports = router;
