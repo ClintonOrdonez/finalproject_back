a506b289557a6e7eeca1beacbe21e8821b9b8999
Clinton Dean Ordoñez <dasmexikaner@Clintons-iMac.local>
2018-11-12 14:55:49 -0700

    Setup teamgestalt.bot@gmail.com as mail sender

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,6 @@
-let router = require("express").Router();
-let User = require("../models/User");
+const router = require("express").Router();
+const User = require("../models/User");
+const nodemailer = require("nodemailer");
 
 // Get a list of users
 // router.get("/", (req, res) => {
@@ -124,17 +125,68 @@ router.put("/resetPassword", (req, res) => {
   let email = req.body.email;
   let date = new Date();
   const expirationMinutes = 15;
-  let expirationDate = date.setMinutes(date.getMinutes() + expirationMinutes);
+  let resetPasswordLink = "http://localhost:3000/resetPassword/";
+  let resetPasswordToken;
+  let resetPasswordExpiration = date.setMinutes(
+    date.getMinutes() + expirationMinutes
+  );
 
   User.findOneAndUpdate(
     { email: email },
-    { resetPasswordExpiration: expirationDate },
+    { resetPasswordExpiration: resetPasswordExpiration },
     { new: true }
   )
     .then(result => {
+      resetPasswordToken = result.password;
+      resetPasswordEmail(email, resetPasswordLink, resetPasswordToken);
       res.send(result);
     })
     .catch(error => res.status(400).send(error));
+
+  console.log(resetPasswordToken);
 });
+
+function resetPasswordEmail(email, resetPasswordLink, resetPasswordToken) {
+  // Create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "teamgestalt.bot@gmail.com", // email
+      pass: "raxqUz-dajmag-pensa4" // password
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: "team-gestalt@no-reply.com", // sender address
+    to: email, // list of receivers
+    subject: "Reset Password Request", // Subject line
+    text:
+      "Please use the following link to reset your password: " +
+      resetPasswordLink +
+      resetPasswordToken +
+      " This is an automated email; please do not respond to this email address.", // plain text body
+    html:
+      "Please use the following link to reset your password:<br/>" +
+      resetPasswordLink +
+      resetPasswordToken +
+      "<br/>This is an automated email; please do not respond to this email address." // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+    // Preview only available when sending through an Ethereal account
+    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  });
+}
 
 module.exports = router;
