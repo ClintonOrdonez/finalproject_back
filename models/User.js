cddcd11c96ede0cc39653a4142109c2530a169b5
Clinton Dean Ordoñez <dasmexikaner@Clintons-iMac.local>
2018-11-16 17:53:36 -0700

    Added /updateTheme route

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 let mongoose = require("mongoose");
 let bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
 
 let UserSchema = new mongoose.Schema(
   {
@@ -17,6 +18,53 @@ UserSchema.methods.encryptPassword = password =>
 
 UserSchema.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.password);
+};
+
+UserSchema.methods.resetPasswordEmail = function(email, resetPasswordToken) {
+  let resetPasswordLink = "http://localhost:3000/resetPassword/";
+  // let resetPasswordLink =
+  //   "https://team-gestalt-app.herokuapp.com/resetPassword/";
+
+  // Create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "teamgestalt.bot@gmail.com", // email
+      pass: "raxqUz-dajmag-pensa4" // password
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: "teamgestalt.bot@gmail.com", // sender address
+    to: email, // list of receivers
+    subject: "Reset Password Request", // Subject line
+    text:
+      "Please use the following link to reset your password: " +
+      resetPasswordLink +
+      resetPasswordToken +
+      " This link will expire in 15 minutes. This is an automated email; please do not respond to this email address.", // plain text body
+    html:
+      "Please use the following link to reset your password:<br/>" +
+      resetPasswordLink +
+      resetPasswordToken +
+      "<br/>This link will expire in 15 minutes.<br/>This is an automated email; please do not respond to this email address." // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+    // Preview only available when sending through an Ethereal account
+    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  });
 };
 
 module.exports = mongoose.model("User", UserSchema);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const nodemailer = require("nodemailer");
 
 // Get a list of users
 // router.get("/", (req, res) => {
@@ -125,9 +124,6 @@ router.put("/resetPassword", (req, res) => {
   let email = req.body.email;
   let date = new Date();
   const expirationMinutes = 15;
-  let resetPasswordLink = "http://localhost:3000/resetPassword/";
-  // let resetPasswordLink =
-  //   "https://team-gestalt-app.herokuapp.com/resetPassword/";
   let resetPasswordToken;
   let resetPasswordExpiration = date.setMinutes(
     date.getMinutes() + expirationMinutes
@@ -140,7 +136,7 @@ router.put("/resetPassword", (req, res) => {
   )
     .then(result => {
       resetPasswordToken = result.password;
-      resetPasswordEmail(email, resetPasswordLink, resetPasswordToken);
+      result.resetPasswordEmail(email, resetPasswordToken);
       res.send(result);
     })
     .catch(error => res.status(400).send(error));
@@ -148,55 +144,23 @@ router.put("/resetPassword", (req, res) => {
   console.log(resetPasswordToken);
 });
 
-function resetPasswordEmail(email, resetPasswordLink, resetPasswordToken) {
-  // Create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "teamgestalt.bot@gmail.com", // email
-      pass: "raxqUz-dajmag-pensa4" // password
-    }
-  });
-
-  // setup email data with unicode symbols
-  let mailOptions = {
-    from: "teamgestalt.bot@gmail.com", // sender address
-    to: email, // list of receivers
-    subject: "Reset Password Request", // Subject line
-    text:
-      "Please use the following link to reset your password: " +
-      resetPasswordLink +
-      resetPasswordToken +
-      " This link will expire in 15 minutes. This is an automated email; please do not respond to this email address.", // plain text body
-    html:
-      "Please use the following link to reset your password:<br/>" +
-      resetPasswordLink +
-      resetPasswordToken +
-      "<br/>This link will expire in 15 minutes.<br/>This is an automated email; please do not respond to this email address." // html body
-  };
-
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log("Message sent: %s", info.messageId);
-    // Preview only available when sending through an Ethereal account
-    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  });
-}
-
 router.post("/findResetPasswordToken", (req, res) => {
   let resetPasswordToken = req.body.resetPasswordToken;
 
   User.findOne({ password: resetPasswordToken })
     .then(result => res.send(result))
     .catch(error => res.status(500).send(error));
+});
+
+router.put("/updateTheme", (req, res) => {
+  let email = req.body.email;
+  let theme = req.body.theme;
+
+  User.findOneAndUpdate({ email: email }, { theme: theme }, { new: true })
+    .then(result => {
+      res.send(result.body.theme);
+    })
+    .catch(error => res.status(400).send(error));
 });
 
 module.exports = router;
